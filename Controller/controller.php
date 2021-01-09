b4956c9f548b4dcf4e6de142480bbf02b1e881e7
Antoine Dumas <antoine.dumas3@etu.univ-lyon1.fr>
2021-01-09 17:44:02 +0100

    ajout de la page d'acceuil

--- Controller/controller.php
+++ Controller/controller.php
@@ -13,4 +13,43 @@ function listProducts()
         $erreur = $e->getMessage();
     }
 }
+
+function listCateg() {
+        // Récupération des catégories de produits
+        try {
+            // on crée un objet référant la classe DialogueBD
+            $undlg = new DialogueBD();
+            $categories = $undlg->getCategories();
+            require_once './View/listeProductView.php';
+        } catch (Exception $e) {
+            $erreur = $e->getMessage();
+        }
+}
+
+function acceuil() {
+        // Récupération des catégories et des produits
+        try {
+            // on crée un objet référant la classe DialogueBD
+            $undlg = new DialogueBD();
+            $products = $undlg->getProducts();
+            $categories = $undlg->getCategories();
+            require_once './View/listeProductView.php';
+        } catch (Exception $e) {
+            $erreur = $e->getMessage();
+        }
+}
+
+function categorie($id) {
+            // Récupération les produits de la catégories passé en param
+            try {
+                // on crée un objet référant la classe DialogueBD
+                $undlg = new DialogueBD();
+                $categories = $undlg->getCategories();
+                $products = $undlg->getProductsCat($id);
+                require_once './View/listeProductCategView.php';
+            } catch (Exception $e) {
+                $erreur = $e->getMessage();
+            }
+}
+
 ?>
