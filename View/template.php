fa5328d0b97a4361abb5927a62d0e140ed24e39a
Antoine Dumas <antoine.dumas3@etu.univ-lyon1.fr>
2021-01-10 14:48:22 +0100

    ajout d'un template pour les pages

---
+++ View/template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html" ; charset="utf-8" />
+    <link rel="stylesheet" href="css/style.css" />
+    <title><?= $title ?></title>
+    <link href="./Bootstrap/css/bootstrap.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/0c2bc87b87.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <?= $content ?>
+    <script src="./Bootstrap/js/jquery-3.5.1.js"></script>
+    <script src="./Bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
