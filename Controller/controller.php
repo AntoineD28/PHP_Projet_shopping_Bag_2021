2139fa3c50751d6557a9a38f1682cba242f26f08
Antoine Dumas <antoine.dumas3@etu.univ-lyon1.fr>
2021-01-10 19:05:47 +0100

    ajout connexion et deconnexion

--- Controller/controller.php
+++ Controller/controller.php
@@ -14,55 +14,102 @@ function listProducts()
     }
 }
 
-function listCateg() {
-        // Récupération des catégories de produits
-        try {
-            // on crée un objet référant la classe DialogueBD
-            $undlg = new DialogueBD();
-            $categories = $undlg->getCategories();
-            require_once './View/listeProductView.php';
-        } catch (Exception $e) {
-            $erreur = $e->getMessage();
+function listCateg()
+{
+    // Récupération des catégories de produits
+    try {
+        // on crée un objet référant la classe DialogueBD
+        $undlg = new DialogueBD();
+        $categories = $undlg->getCategories();
+        require_once './View/listeProductView.php';
+    } catch (Exception $e) {
+        $erreur = $e->getMessage();
+    }
+}
+
+function TousProduits()
+{
+    // Récupération des catégories et des produits
+    try {
+        // on crée un objet référant la classe DialogueBD
+        $undlg = new DialogueBD();
+        $products = $undlg->getProducts();
+        $categories = $undlg->getCategories();
+        require_once './View/listeProductView.php';
+    } catch (Exception $e) {
+        $erreur = $e->getMessage();
+    }
+}
+
+function categorie($id)
+{
+    // Récupération les produits de la catégories passé en param
+    try {
+        // on crée un objet référant la classe DialogueBD
+        $undlg = new DialogueBD();
+        $categories = $undlg->getCategories();
+        $products = $undlg->getProductsCat($id);
+        $name = $undlg->getNomCat($id);
+        require_once './View/listeProductCategView.php';
+    } catch (Exception $e) {
+        $erreur = $e->getMessage();
+    }
+}
+
+function accueil()
+{
+    // Récupération des catégories
+    try {
+        // on crée un objet référant la classe DialogueBD
+        $undlg = new DialogueBD();
+        $categories = $undlg->getCategories();
+        require_once './View/listeCategView.php';
+    } catch (Exception $e) {
+        $erreur = $e->getMessage();
+    }
+}
+
+function inscription()
+{
+}
+
+function connexion()
+{
+    // Récupération de l'utilisateur
+    try {
+        // on crée un objet référant la classe DialogueBD
+        $undlg = new DialogueBD();
+        $user = $undlg->getUtilisateur($_POST['login'], $_POST['mdp']);
+        $categories = $undlg->getCategories();
+        foreach($user as $u){
+            $id = $u['customer_id'];
+            $username = $u['username'];
         }
-}
-
-function TousProduits() {
-        // Récupération des catégories et des produits
-        try {
-            // on crée un objet référant la classe DialogueBD
-            $undlg = new DialogueBD();
-            $products = $undlg->getProducts();
-            $categories = $undlg->getCategories();
-            require_once './View/listeProductView.php';
-        } catch (Exception $e) {
-            $erreur = $e->getMessage();
-        }
-}
-
-function categorie($id) {
-            // Récupération les produits de la catégories passé en param
-            try {
-                // on crée un objet référant la classe DialogueBD
-                $undlg = new DialogueBD();
-                $categories = $undlg->getCategories();
-                $products = $undlg->getProductsCat($id);
-                $name = $undlg->getNomCat($id);
-                require_once './View/listeProductCategView.php';
-            } catch (Exception $e) {
-                $erreur = $e->getMessage();
-            }
-}
-
-function accueil() {
-        // Récupération des catégories
-        try {
-            // on crée un objet référant la classe DialogueBD
-            $undlg = new DialogueBD();
-            $categories = $undlg->getCategories();
+        if (isset($id) && isset($username)) {
+            $_SESSION['ID'] = $id;
+            $_SESSION['NAME'] = $username;
             require_once './View/listeCategView.php';
-        } catch (Exception $e) {
-            $erreur = $e->getMessage();
         }
+        else {
+            $_SESSION['authOK'] = false;
+            require_once './View/connexionView.php';
+        }
+    } catch (Exception $e) {
+        $erreur = $e->getMessage();
+    }
 }
 
-?>
+function deconnexion()
+{
+    // l'utilisateur s'est déconnecté on détruit la session
+    session_destroy();
+    // Récupération des catégories
+    try {
+        // on crée un objet référant la classe DialogueBD
+        $undlg = new DialogueBD();
+        $categories = $undlg->getCategories();
+        require_once './View/listeCategView.php';
+    } catch (Exception $e) {
+        $erreur = $e->getMessage();
+    }
+}
