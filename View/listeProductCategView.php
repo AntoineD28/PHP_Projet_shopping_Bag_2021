2804200e445a8397e1d063663cd2b669b5a268d3
Antoine Dumas <antoine.dumas3@etu.univ-lyon1.fr>
2021-01-10 14:49:43 +0100

    contient seulement le body de la page

--- View/listeProductCategView.php
+++ View/listeProductCategView.php
@@ -1,37 +1,60 @@
-<!DOCTYPE html>
-<html>
+<?php $title = $name[0]['name'];?>
+<?php ob_start(); ?>
 
-<head>
-    <meta http-equiv="Content-Type" content="text/html" ; charset="utf-8" />
-    <link rel="stylesheet" href="css/style.css" />
-    <title> Menu principal </title>
-</head>
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #414141;">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="./index.php">
+            <img src="./productimages/biscuitNoel.png" alt="" width="60" height="48" class="d-inline-block">
+            Teatea Henry
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Catégories</a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <?php
+                        foreach ($categories as $cat) {
+                            echo '<li><a class="dropdown-item" href="index.php?action=' . $cat['id'] . '">' . $cat['name'] . '</a></li>';
+                        }
+                        ?>
+                    </ul>
+                </li>
+            </ul>
+            <a class="btn btn-warning me-2" href="#" role="button"><i class="fas fa-user"></i> Connexion</a>
 
-<body>
-    <div class="header">
-        <h1>TeaTea Henry</h1> 
+            <a class="btn btn-dark text-warning" href="#" role="button"><i class="fas fa-shopping-cart"></i> Mon Panier</a>
+        </div>
     </div>
+</nav>
 
-    <nav>
-        <ul>
-            <?php
-                foreach($categories as $cat){
-                    echo '<li><a href="index.php?action='. $cat['id'] .'">'. $cat['name'] .'</a></li>';
-                }
-            ?>
-        </ul>
-    </nav>
-
+<div class="text-center">
     <?php
-        foreach($products as $p){
-            echo '<div>';
-            echo "<p>". $p['name'] ."</p>";
-            echo "<p>". $p['description'] ."</p>";
-            echo '<img src="./productimages/'. $p['image'] .'"  alt="">';
-            echo "<p>". $p['price'] ."</p>";
-            echo "</div>"; 
-        }
+    echo "<h1> Nos ". $name[0]['name'] ."</h1>";
+    echo $_SESSION['test'];    
     ?>
-</body>
+</div>
 
-</html>
+<div class="container">
+    <div class="row justify-content-md-center">
+        <?php
+        foreach ($products as $p) {
+            echo '<div class="card col-md-auto me-2 mb-2" style="width: 30rem;">
+                <img src="./productimages/' . $p['image'] . '" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">' . $p['name'] . '</h5>
+                    <p class="card-text">' . $p['description'] . '</p>
+                    <p class="card-text">' . $p['price'] . ' €</p>
+                    <a href="#" class="btn btn-warning">Buy</a>
+                </div>
+            </div>';
+        }
+        ?>
+    </div>
+</div>
+<?php
+$content = ob_get_clean();
+require_once 'template.php';
+?>
--- View/listeProductView.php
+++ View/listeProductView.php
@@ -1,67 +1,53 @@
-<!DOCTYPE html>
-<html>
+<?php $title = 'TeaTea';?>
+ <?php ob_start();?>
 
-<head>
-    <meta http-equiv="Content-Type" content="text/html" ; charset="utf-8" />
-    <link rel="stylesheet" href="css/style.css" />
-    <title> Menu principal </title>
-    <link href="./Bootstrap/css/bootstrap.css" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/0c2bc87b87.js" crossorigin="anonymous"></script>
-</head>
-
-<body>
-    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #414141;">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="./productimages/biscuitNoel.png" alt="" width="60" height="48" class="d-inline-block">
-                Teatea Henry
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav me-auto">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Catégories</a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <?php
-                            foreach($categories as $cat){
-                                echo '<li><a class="dropdown-item" href="index.php?action='. $cat['id'] .'">'. $cat['name'] .'</a></li>';
+ <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #414141;">
+     <div class="container-fluid">
+         <a class="navbar-brand" href="./index.php">
+             <img src="./productimages/biscuitNoel.png" alt="" width="60" height="48" class="d-inline-block">
+             Teatea Henry
+         </a>
+         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+             <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="navbarNav">
+             <ul class="navbar-nav me-auto">
+                 <li class="nav-item dropdown">
+                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Catégories</a>
+                     <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                         <?php
+                            foreach ($categories as $cat) {
+                                echo '<li><a class="dropdown-item" href="index.php?action=' . $cat['id'] . '">' . $cat['name'] . '</a></li>';
                             }
-                        ?>
-                        </ul>
-                    </li>
-                </ul>
-                <a class="btn btn-warning me-2" href="#" role="button"><i class="fas fa-user"></i> Connexion</a>
-                
-                <a class="btn btn-dark text-warning" href="#" role="button"><i class="fas fa-shopping-cart"></i> Mon Panier</a>
-            </div>
-        </div>
-    </nav>
+                            ?>
+                     </ul>
+                 </li>
+             </ul>
+             <a class="btn btn-warning me-2" href="#" role="button"><i class="fas fa-user"></i> Connexion</a>
 
-    <nav>
-        <ul>
-            <?php
-                foreach($categories as $cat){
-                    echo '<li><a href="index.php?action='. $cat['id'] .'">'. $cat['name'] .'</a></li>';
-                }
+             <a class="btn btn-dark text-warning" href="#" role="button"><i class="fas fa-shopping-cart"></i> Mon Panier</a>
+         </div>
+     </div>
+ </nav>
+
+ <div class="container">
+     <div class="row justify-content-md-center">
+         <?php
+            foreach ($products as $p) {
+                echo '<div class="card col-md-auto me-2 mb-2" style="width: 30rem;">
+                <img src="./productimages/' . $p['image'] . '" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">' . $p['name'] . '</h5>
+                    <p class="card-text">' . $p['description'] . '</p>
+                    <p class="card-text">' . $p['price'] . ' €</p>
+                    <a href="#" class="btn btn-primary">Buy</a>
+                </div>
+            </div>';
+            }
             ?>
-        </ul>
-    </nav>
-
-    <?php
-        foreach($products as $p){
-            echo '<div>';
-            echo "<p>". $p['name'] ."</p>";
-            echo "<p>". $p['description'] ."</p>";
-            echo '<img src="./productimages/'. $p['image'] .'"  alt="">';
-            echo "<p>". $p['price'] ."</p>";
-            echo "</div>"; 
-        }
+     </div>
+ </div>
+ <?php
+    $content = ob_get_clean();
+    require_once 'template.php';
     ?>
-    
-    <script src="./Bootstrap/js/jquery-3.5.1.js"></script>
-    <script src="./Bootstrap/js/bootstrap.min.js"></script>
-</body>
-
-</html>
