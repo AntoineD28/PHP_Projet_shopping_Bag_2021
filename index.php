82504dcdad26d1f4d2c74ca5189e6cbaf0544a82
Antoine Dumas <antoine.dumas3@etu.univ-lyon1.fr>
2021-01-20 17:24:43 +0100

    maj

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <?php
-    require_once('./Controller/controller.php');
-    session_start();
-    
-    //$param = explode('/', $_GET['action']);
-    if(isset($_GET['action'])) {
-        if(strlen($_GET['action']) == 1){
-            categorie($_GET['action']);
-        }
-        else {
-            $_GET['action']();
-        }
+require_once('./Controller/controller.php');
+session_start();
+
+//$param = explode('/', $_GET['action']);
+if (isset($_GET['action'])) {
+    if (strlen($_GET['action']) == 1) {
+        categorie($_GET['action']);
+    } else if (isset($_GET['id'])) {
+        $_GET['action']($_GET['id']);
+    } else {
+        $_GET['action']();
     }
-    else {
-        accueil();
-    }
-?>
+} else {
+    accueil();
+}
