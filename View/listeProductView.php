0644e761bbea1879fee31ce49b6bdbfdf19a4d5d
Antoine Dumas <antoine.dumas3@etu.univ-lyon1.fr>
2021-01-20 17:25:18 +0100

    bouton buy semi-focntionel

--- View/listeProductView.php
+++ View/listeProductView.php
@@ -52,7 +52,26 @@
                     <h5 class="card-title">' . $p['name'] . '</h5>
                     <p class="card-text">' . $p['description'] . '</p>
                     <p class="card-text">' . $p['price'] . ' €</p>
-                    <a href="#" class="btn btn-primary">Buy</a>
+                    <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#static'.$p['name'].'">
+                        Buy
+                    </button>
+                </div>
+            </div>
+            <div class="modal fade" id="static'.$p['name'].'" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-dark text-warning" data-bs-dismiss="modal">Annuler</button>
+                            <button type="button" class="btn btn-warning">Confirmer</button>
+                        </div>
+                    </div>
                 </div>
             </div>';
         }
