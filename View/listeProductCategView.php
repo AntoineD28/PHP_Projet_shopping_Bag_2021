b4587c7f26e9a89f92f8c5bceea6a9974d3bef26
Antoine Dumas <antoine.dumas3@etu.univ-lyon1.fr>
2021-01-20 17:25:39 +0100

    bouton buy semi-fonctionel

--- View/listeProductCategView.php
+++ View/listeProductCategView.php
@@ -51,16 +51,40 @@
 <div class="container">
     <div class="row justify-content-md-center">
         <?php
-        foreach ($products as $p) {
+        $i = 0;
+         foreach ($products as $p) {
             echo '<div class="card col-md-auto me-2 mb-2" style="width: 30rem;">
                 <img src="./productimages/' . $p['image'] . '" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">' . $p['name'] . '</h5>
                     <p class="card-text">' . $p['description'] . '</p>
                     <p class="card-text">' . $p['price'] . ' €</p>
-                    <a href="#" class="btn btn-warning">Buy</a>
+                    <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#static'.$i.'">
+                        Buy
+                    </button>
+                </div>
+            </div>
+            <div class="modal fade" id="static'.$i.'" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="'.$i.'Label" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="'.$i.'Label">' . $p['name'] . '</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <form action="index.php?action=AjoutPanier&id='.$p['id'].'" method="post">
+                                <label for="Quantity"> Veuillez selectionner un quantité : </label>
+                                <input type="number" id="Quantity" name="Quantity" min="1" max="100" required>
+                        </div>
+                        <div class="modal-footer">
+                                <button type="button" class="btn btn-dark text-warning" data-bs-dismiss="modal">Annuler</button>
+                                <input type="submit" class="btn btn-warning" value="Confirmer">
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>';
+        $i++;
         }
         ?>
     </div>
